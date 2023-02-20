2a340f7a672c71bffbbbd1cf1cb1fc193aca8283
jd127 <jd127@users.noreply.github.com>
2023-02-20 16:45:02 +0100

    add new studentapi running jetty

--- 06_studentapi_jetty/src/main/java/org/example/springweb/model/Student.java
+++ 06_studentapi_jetty/src/main/java/org/example/springweb/model/Student.java
@@ -1,13 +1,15 @@
 package org.example.springweb.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Student {
 
     @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private int testscore;
