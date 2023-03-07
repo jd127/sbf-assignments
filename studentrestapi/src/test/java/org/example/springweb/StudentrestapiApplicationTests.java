2bc3291472121d864f394493c68682ad1720375e
jd127 <jd127@users.noreply.github.com>
2023-03-07 08:19:29 +0100

    copy 06_studentrestapi into studentrestapi

---
+++ studentrestapi/src/test/java/org/example/springweb/StudentrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
